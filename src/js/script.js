b7e6ab2032249b816967d4ede2e8a29f02b162b7
kherma <kamil.herma01@gmail.com>
2025-10-13 19:26:35 +0200

    feat: display selected article on title link click

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const removeActiveClass = (query) => {
+  const activeLinks = document.querySelectorAll(query);
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+};
+
+const titleClickHandler = function (event) {
+  event.preventDefault();
+  const clickedElement = this;
+
+  /* remove class 'active' from all article links  */
+  removeActiveClass('.titles a.active');
+
+  /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+
+  /* remove class 'active' from all articles */
+  removeActiveClass('.posts article.active');
+
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+
+  /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+};
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
